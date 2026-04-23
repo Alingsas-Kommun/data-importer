400b27e2beae65cfe6b3cc02b24b2d60ec61b2d2
Michael Claesson <michael.claesson@consid.se>
2026-04-23 17:04:39 +0200

    Release build for version 1.0.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'michaelclaesson/data-importer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8beaa3731a1d3403a08411a7272ae79db14f4d85',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'michaelclaesson/data-importer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8beaa3731a1d3403a08411a7272ae79db14f4d85',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
