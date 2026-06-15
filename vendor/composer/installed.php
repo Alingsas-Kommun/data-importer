2676bac5d0bee54646656d289515488d75a2420f
Michael Claesson <michael.claesson@consid.se>
2026-06-15 12:09:31 +0200

    Release build for version 1.0.4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alingsas-kommun/data-importer',
+        'pretty_version' => '1.0.4',
+        'version' => '1.0.4.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alingsas-kommun/data-importer' => array(
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
